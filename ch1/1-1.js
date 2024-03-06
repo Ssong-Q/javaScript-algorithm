fd5b386ac4913f2924822a736be2301076f4e51b
Ssong-Q <skdmlrh12@gmail.com>
2024-03-06 15:01:11 +0900

    [1-1] 세 수 중 최솟값

--- ch1/1-1.js
+++ ch1/1-1.js
@@ -1,3 +1,9 @@
-function solution() {}
+function solution(A, B, C) {
+  const arr = [A, B, C];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < arr[i + 1]) arr[(i, i + 1)] = arr[(i + 1, i)];
+  }
+  return arr[arr.length - 1];
+}
 
-console.log(solution());
+console.log(solution(6, 5, 11));
