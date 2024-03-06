ed30b2f987ab1024b46fce5404976c1450a971aa
Ssong-Q <skdmlrh12@gmail.com>
2024-03-07 01:42:24 +0900

    [1-11] 대문자 찾기

---
+++ ch1/1-11.js
@@ -0,0 +1,22 @@
+function solution(word) {
+  let answer = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i].charCodeAt(0) >= 65 && word[i].charCodeAt(0) <= 90) answer++;
+  }
+
+  return answer;
+}
+
+console.log(solution("KoreaTimeGood"));
+
+function teacherSolution(s) {
+  let answer = 0;
+  for (let x of s) {
+    //let num=x.charCodeAt();
+    //if(num>=65 && num<=90) answer++;
+    if (x === x.toUpperCase()) answer++;
+  }
+
+  return answer;
+}
