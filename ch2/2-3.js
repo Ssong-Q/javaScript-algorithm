988f2ca69ad022399800fa35b4e86560add797ca
James Song <skdmlrh12@gmail.com>
2024-03-08 20:56:34 +0900

    [2-3] 가위 바위 보, 강사님 풀이 추가

--- ch2/2-3.js
+++ ch2/2-3.js
@@ -30,14 +30,15 @@ function solution(A, B) {
 
 console.log(solution([2, 3, 3, 1, 3], [1, 1, 2, 2, 3]));
 
-function teacherSolution(arr) {
-  let answer = 1,
-    max = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > max) {
-      answer++;
-      max = arr[i];
-    }
+function teacherSolution(a, b) {
+  let answer = "";
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === b[i]) answer += "D ";
+    else if (a[i] === 1 && b[i] === 3) answer += "A ";
+    else if (a[i] === 2 && b[i] === 1) answer += "A ";
+    else if (a[i] === 3 && b[i] === 2) answer += "A ";
+    else answer += "B ";
   }
+
   return answer;
 }
